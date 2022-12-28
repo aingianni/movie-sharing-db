5bb15a96da6d5359ff0a996414c595627596c39f
aingianni92 <ingianni.anthony@gmail.com>
2022-12-27 21:41:10 -0500

    added carousel src link to readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 This app is designed as a social space for users to add, collect and share their favorite movies utilizing a 3rd party API for the movie data.
 
 [Trello Link](https://trello.com/b/GkkB1WfQ/project-3)
+
+## Links to Resources Used
+
+[How to Build a Carousel](https://blog.bitsrc.io/simple-carousel-in-react-2aac73887243)
--- src/components/Profile/FavoriteMovies.js
+++ src/components/Profile/FavoriteMovies.js
@@ -4,14 +4,14 @@ export default function FavoriteMovies ({ getMovies, movies, user }) {
   const [index, setIndex] = useState(0)
 
   const scroll = () => {
-    if (index === movies.length-1) {
+    if (index === movies.length - 1) {
       return setIndex(0)
     }
     return setIndex(index + 1)
   }
 
   useEffect(() => {
-    const interval = setInterval(() => {scroll()}, 3000)
+    const interval = setInterval(() => { scroll() }, 3000)
     return () => clearInterval(interval)
   })
 
@@ -22,9 +22,9 @@ export default function FavoriteMovies ({ getMovies, movies, user }) {
           movies
             ? movies.map((movie) => {
               return (
-              <div className='carousel-item' style={{transform: `translate(-${index * 100}%)`}} key={movie._id}>
-                <img src={movie.Poster} />
-              </div>
+                <div className='carousel-item' style={{ transform: `translate(-${index * 100}%)` }} key={movie._id}>
+                  <img src={movie.Poster} />
+                </div>
               )
             })
             : ''
